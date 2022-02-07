c693fa7203386a668559a2ab267da46cefcade37
chongjunxian <chongjx-wm19@student.tarc.edu.my>
2022-02-07 22:53:40 +0800

    Version1_Practical 2.4

---
+++ app/src/main/java/com/example/databindingdemo/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.databindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import androidx.databinding.DataBindingUtil
+import com.example.databindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val s = Student("W123","John")
+
+        binding.myData = s
+        //binding.tvTextFieldName.text = "W123456"
+
+
+        binding.btnUpdate.setOnClickListener(){
+            s.studentName = "Alex"
+
+            binding.apply {
+                invalidateAll()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/databindingdemo/Student.kt
@@ -0,0 +1,6 @@
+package com.example.databindingdemo
+
+data class Student(
+    var studentID :String ="",
+    var studentName :String =""
+)
